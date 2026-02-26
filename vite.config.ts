19d35ff2db7183dfc38fc823480e0e1cc7dc9506
Capplequoppe <contact@hallgrenstudios.com>
2026-02-26 21:07:11 +0100

    fix: remove alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
-
 export default defineConfig({
   base: "/thai-script/",
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@services": path.resolve(__dirname, ".."),
-    },
-  },
 });
